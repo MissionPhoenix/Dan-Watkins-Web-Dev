e11772a804823e2307f51ed9e7aac3f3e464e4c8
MissionPhoenix <journeyofthehermit@googlemail.com>
2020-06-03 19:47:47 +0100

    trimming code and changing some classes in index.php

--- wordpress/wp-content/themes/missionphoenix/index.php
+++ wordpress/wp-content/themes/missionphoenix/index.php
@@ -8,11 +8,12 @@
     <div class="card">
         <div class="card-body">
             <h3 class="card-title"><?php the_title();?></h3>
-            <div class="card-text"><?php the_excerpt();?>
+            <div class="card-text">
+            <span class='post-exerpt'><?php the_excerpt();?></span>
             <span class='author-posts-link'><?php echo get_the_author_posts_link(); ?><span>
             <span class='post-date'><?php echo get_the_date(); ?><span>
             </div>
-            <a href="<?php the_permalink();?>" class="btn btn-success">Read More</a>
+            <a href="<?php the_permalink();?>" class="read-more-button">Read More</a>
         </div>
     </div>
 <?php endwhile; endif;// have_posts()?>
