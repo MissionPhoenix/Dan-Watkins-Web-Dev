3c90f17eeb998cd288e37ca5812ed28bb9bcdd1c
MissionPhoenix <journeyofthehermit@googlemail.com>
2020-06-11 19:01:52 +0100

    shortening code

--- wordpress/wp-content/themes/missionphoenix/index.php
+++ wordpress/wp-content/themes/missionphoenix/index.php
@@ -10,8 +10,8 @@
                 <h3 class="card-title"><?php the_title();?></h3>
                 <div class="card-text">
                 <span class='post-exerpt'><?php the_excerpt();?></span>
-                <span class='author-posts-link'><?php echo get_the_author_posts_link(); ?><span>
-                <span class='post-date'><?php echo get_the_date(); ?><span>
+                <span class='author-posts-link'><?php the_author_posts_link(); ?><span>
+                <span class='post-date'><?php the_date(); ?><span>
                 </div>
                 <a href="<?php the_permalink();?>" class="read-more-button">Read More</a>
             </div>
