789e73781b577d9bbfd0aca8eaa74de0b1624339
MissionPhoenix <journeyofthehermit@googlemail.com>
2020-06-11 19:09:02 +0100

    added new files after scss setup

--- wordpress/wp-content/themes/missionphoenix/functions.php
+++ wordpress/wp-content/themes/missionphoenix/functions.php
@@ -3,7 +3,7 @@
 function load_stylesheets()
 {
 
-    wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
 
     wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
@@ -15,7 +15,7 @@ add_action('wp_enqueue_scripts', 'load_stylesheets');
 function load_javascripts()
 {
 
-    wp_register_script('phoenixjs', get_template_directory_uri() . '/bootstrap/js/scripts.js', array(), false, true);
+    wp_register_script('phoenixjs', get_template_directory_uri() . '/js/scripts.js', array(), false, true);
     wp_enqueue_script('phoenixjs');
 
 
