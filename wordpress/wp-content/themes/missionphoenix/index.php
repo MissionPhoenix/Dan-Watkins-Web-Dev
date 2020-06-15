4f89ec642f83c382e84ac3d55da9efd39780769a
MissionPhoenix <journeyofthehermit@googlemail.com>
2020-06-15 13:56:03 +0100

    added categories to all posts

--- wordpress/wp-content/themes/missionphoenix/index.php
+++ wordpress/wp-content/themes/missionphoenix/index.php
@@ -10,8 +10,9 @@
                 <h3 class="card-title"><?php the_title();?></h3>
                 <div class="card-text">
                 <span class='post-exerpt'><?php the_excerpt();?></span>
-                <span class='author-posts-link'><?php the_author_posts_link(); ?><span>
-                <span class='post-date'><?php the_date(); ?><span>
+                <span class='author-posts-link'><?php the_author_posts_link(); ?></span>
+                <span class='post-date'><?php the_date(); ?></span>
+                <span class='categories'><?php the_category(', ');?></span>
                 </div>
                 <a href="<?php the_permalink();?>" class="read-more-button">Read More</a>
             </div>
