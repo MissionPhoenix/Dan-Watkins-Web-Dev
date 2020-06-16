fe7e8f72a894e388d2109bd0a431a89bd905b18f
MissionPhoenix <journeyofthehermit@googlemail.com>
2020-06-16 17:12:36 +0100

    added and started main js file

---
+++ wordpress/wp-content/themes/missionphoenix/js/scripts.js
@@ -0,0 +1,8 @@
+/* JavaScript for Mission Phoenix theme
+* author Dan Watkins danwatkins82@gmail.com
+* 16/06/2020
+*/
+//Check if the screen is landscape or portrait */
+if (window.innerHeight > window.innerWidth) {
+    var orientation = 'landscape';
+} else var orientation = 'portrait';
